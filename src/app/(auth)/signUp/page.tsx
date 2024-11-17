798e9bc23081f26fefe329ec4cff03b64d7114a7
Eric <eric.gro.2000@gmail.com>
2024-11-17 17:11:01 +0100

    change file extention to ts
    changed name

--- src/app/(auth)/signUp/page.tsx
+++ src/app/(auth)/signUp/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 
 import { getCurrent } from "@/features/auth/queries";
-import { SignUpCard } from "@/features/auth/components/zignUpCard";
+import { SignUpCard } from "@/features/auth/components/signUpCard";
 
 const SignUpPage = async () => {
   const user = await getCurrent();
