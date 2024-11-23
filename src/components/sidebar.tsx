30a7439efd073916f8fa74d43aff139fbc0e1231
Eric <eric.gro.2000@gmail.com>
2024-11-23 19:03:04 +0100

    added naviagtion to sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 import { DottedSeparator } from "./dottedSeparator";
 // import { Projects } from "./projects";
-// import { Navigation } from "./navigation";
+import { Navigation } from "./navigation";
 // import { WorkspaceSwitcher } from "./workspaceSwitcher";
 
 export const Sidebar = () => {
@@ -13,11 +13,11 @@ export const Sidebar = () => {
         <Image src="/logo.svg" alt="logo" width={32} height={32} />
       </Link>
       <DottedSeparator className="my-4" />
-      {/* <WorkspaceSwitcher />
+      {/* <WorkspaceSwitcher /> */}
       <DottedSeparator className="my-4" />
       <Navigation />
       <DottedSeparator className="my-4" />
-      <Projects /> */}
+      {/* <Projects /> */}
     </aside>
   );
 };
