20900e4e231b72d9d460a74903700ec2dfc3db43
Eric <eric.gro.2000@gmail.com>
2024-12-03 01:49:36 +0100

    added k letter to file name + uncomment it imported component from this file in sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { DottedSeparator } from "./dottedSeparator";
 // import { Projects } from "./projects";
 import { Navigation } from "./navigation";
-// import { WorkspaceSwitcher } from "./workspaceSwitcher";
+import { WorkspaceSwitcher } from "./workspaceSwitcher";
 
 export const Sidebar = () => {
   return (
@@ -13,7 +13,7 @@ export const Sidebar = () => {
         <Image src="/logo.svg" alt="logo" width={32} height={32} />
       </Link>
       <DottedSeparator className="my-4" />
-      {/* <WorkspaceSwitcher /> */}
+      <WorkspaceSwitcher />
       <DottedSeparator className="my-4" />
       <Navigation />
       <DottedSeparator className="my-4" />
