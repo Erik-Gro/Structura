0c5a5f868041da65f9074ec5827bec2fbd3356af
Eric <eric.gro.2000@gmail.com>
2024-12-04 18:04:29 +0100

    changed logo source

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -5,12 +5,13 @@ import { DottedSeparator } from "./dottedSeparator";
 // import { Projects } from "./projects";
 import { Navigation } from "./navigation";
 import { WorkspaceSwitcher } from "./workspaceSwitcher";
+import { logo } from "@/config";
 
 export const Sidebar = () => {
   return (
     <aside className="h-full bg-neutral-100 p-4 w-full">
       <Link href="/">
-        <Image src="/logo.svg" alt="logo" width={32} height={32} />
+        <Image src={logo} alt="logo" width={32} height={32} />
       </Link>
       <DottedSeparator className="my-4" />
       <WorkspaceSwitcher />
