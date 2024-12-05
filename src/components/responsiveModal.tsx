d196dfb8278c59b92e0f5c0b3eb2cb3ed4513730
Eric <eric.gro.2000@gmail.com>
2024-12-05 12:03:53 +0100

    screenreader imprrovment

--- src/components/responsiveModal.tsx
+++ src/components/responsiveModal.tsx
@@ -1,25 +1,22 @@
-"use client";
-
 import { useMedia } from "react-use";
-
-import { Dialog, DialogContent } from "@/components/ui/dialog";
-
+import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import { Drawer, DrawerContent } from "@/components/ui/drawer";
-
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import dynamic from "next/dynamic";
-
 import { useEffect, useState } from "react";
 
 interface ResponsiveModalProps {
   children: React.ReactNode;
   open: boolean;
   onOpenChange: (open: boolean) => void;
+  title?: string;
 }
 
 const ResponsiveModal = ({
   children,
   open,
   onOpenChange,
+  title = "Dialog Title",
 }: ResponsiveModalProps) => {
   const [_isMounted, setIsMounted] = useState(false);
 
@@ -35,6 +32,9 @@ const ResponsiveModal = ({
     return (
       <Dialog open={open} onOpenChange={onOpenChange}>
         <DialogContent className="w-full sm:max-w-lg p-0 border-none overflow-y-auto hide-scrollbar max-h-[85vh]">
+          <DialogTitle>
+            <VisuallyHidden>{title}</VisuallyHidden>
+          </DialogTitle>
           {children}
         </DialogContent>
       </Dialog>
